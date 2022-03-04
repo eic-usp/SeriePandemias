4de9fa9eb88d03cf800c5ae53207e59611dfafa3
Isaias1717 <icoliveira@usp.br>
2022-03-04 10:53:54 -0300

    Imagem index

--- assets/js/media.js
+++ assets/js/media.js
@@ -5,17 +5,10 @@ var peste = document.getElementById("peste");
 var tifo = document.getElementById("tifo");
 var variola = document.getElementById("variola");
 var repetir = document.getElementById("novamente");
-var repeat1 = document.getElementsByClassName("play1");
+var repeat1 = document.getElementById("play1");
 
 covid.onended = event => {
     window.location = "fim.html"
-}
-covid.onplay = event => {
-  function insert() {
-        covid.classList.add("play1")
-        console.log(covid.classList)
-    }
-    insert()
 }
 colera.onended = event => {
     window.location = "fim.html"
@@ -31,12 +24,4 @@ tifo.onended = event => {
 }
 variola.onended = event => {
     window.location = "fim.html"
-}
-repetir.onclick = event =>{
-    repeat().then(function(){
-        repeat1.classList.remove("play1")
-    })
-}
-function repeat(){
-    repeat1.play();
 }
