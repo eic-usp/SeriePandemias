52e63be5d5c66afd0941df0cafd2ad56de06ef1b
Nata-botelho <icoliveira@usp.br>
2022-03-11 16:48:11 -0300

    Ajustes gerais

--- assets/js/media.js
+++ assets/js/media.js
@@ -4,28 +4,27 @@ var gripe = document.getElementById("gripe");
 var peste = document.getElementById("peste");
 var tifo = document.getElementById("tifo");
 var variola = document.getElementById("variola");
-var repetir = document.getElementById("novamente");
-var repeat1 = document.getElementById("play1");
-var covid1 = docuument.getElementById("covid");
+
+document.getElementById("Replay").disabled = true;
 
 covid.onended = event => {
-    window.location = "fim.html"
+  document.getElementById("Replay").disabled = false;
 }
 colera.onended = event => {
-    window.location = "fim.html"
+    document.getElementById("Replay").disabled = false;
 }
 gripe.onended = event => {
-    window.location = "fim.html"
+    document.getElementById("Replay").disabled = false;
 }
 peste.onended = event => {
-    window.location = "fim.html"
+    document.getElementById("Replay").disabled = false;
 }
 tifo.onended = event => {
-    window.location = "fim.html"
+    document.getElementById("Replay").disabled = false;
 }
 variola.onended = event => {
-    window.location = "fim.html"
+    document.getElementById("Replay").disabled = false;
 }
 covid.onclick = event =>{
-    window.location = "covid.html"
+    document.getElementById("Replay").disabled = false;
 }
